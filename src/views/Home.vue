6dc54947e21a2ba10a4aa75fb4902bc80a4681a3
Milo Rupp <badtzmaru139@gmail.com>
2019-05-12 00:29:55 -0700

    finally getting somewhere

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-html="resBody"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import fetch from 'node-fetch';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
   },
+  data() {
+    return {
+      resBody: '',
+    };
+  },
+  mounted() {
+    const self = this;
+    const term = '201930';
+    const subject = 'CSCI';
+    const data = `sel_subj=dummy&sel_subj=dummy&sel_gur=dummy&sel_gur=dummy&sel_attr=dummy&sel_site=dummy&sel_day=dummy&sel_open=dummy&sel_crn=&term=${term}&sel_gur=All&sel_attr=All&sel_site=All&sel_subj=${subject}&sel_inst=ANY&sel_crse=&begin_hh=0&begin_mi=A&end_hh=0&end_mi=A&sel_cdts=%25`;
+
+    const myRequest = new Request('https://cors-anywhere.herokuapp.com/https://admin.wwu.edu/pls/wwis/wwsktime.ListClass', {
+      method: 'POST',
+      body: data,
+    });
+
+    fetch(myRequest).then(response => response.text())
+      .then((text) => {
+        console.log(text);
+        self.resBody = text;
+      });
+  },
+
 };
 </script>
