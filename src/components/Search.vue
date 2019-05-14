70b06b09e602fcf850d29e168171c9c6395aef02
Milo Rupp <badtzmaru139@gmail.com>
2019-05-14 14:33:48 -0700

    Add basic search page

---
+++ src/components/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>Search for a class</h3>
+    <input type="text" v-model="Subject" placeholder="CSCI" required>
+    <select v-model="Term" autocomplete="off" required>
+      <option value="Winter">Winter</option>
+      <option value="Spring">Spring</option>
+      <option value="Summer">Summer</option>
+      <option value="Fall" selected>Fall</option>
+    </select>
+    <button @click="search()">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {
+  },
+  data() {
+    return {
+      Subject: '',
+      Term: '',
+    };
+  },
+  methods: {
+    search() {
+      if (this.Term && this.Subject) {
+        this.$router.push({ name: 'results', params: { term: this.Term, subject: this.Subject } });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+div {
+  width: 80%;
+  height: 200px;
+  margin: auto;
+  box-shadow: 1px 3px 10px rgba(0, 0, 0, 0.2);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <router-link to="/results/Summer/CSCI">Results</router-link>
+    <!-- <router-link to="/results/Summer/CSCI">Results</router-link> -->
+    <Search/>
   </div>
 </template>
+<script>
+import Search from '@/components/Search.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Search,
+  },
+  props: {
+  },
+};
+</script>
