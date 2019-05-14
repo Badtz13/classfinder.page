a97eacbc2e7be7c568c0ba67ae9aef6d9419db5e
Milo Rupp <badtzmaru139@gmail.com>
2019-05-14 16:30:24 -0700

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      Welcome! This site is an attempt at replacing the
+      old and bad looking WWU classfinder page.
+      This is still a work in progress, as the way this site
+      fetches data is by scraping the other site.
+      Please report any issues or suggest features on our
+      <a href="https://github.com/MiloCodes/WWU-Classfinder-Replacement/issues/new">github.</a>
+    </p>
   </div>
 </template>
+
+<style scoped>
+p {
+  width: 500px;
+  margin:auto;
+}
+</style>
