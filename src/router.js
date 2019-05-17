b35e16e613379df4a91edc9256b53ce2a094c51b
Milo Rupp <badtzmaru139@gmail.com>
2019-05-17 11:36:58 -0700

    Move everything to home page

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/home/:term?/:subject?',
       name: 'home',
       component: Home,
     },
@@ -20,13 +20,6 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
-    {
-      path: '/results',
-      name: 'results',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Results.vue'),
-    },
+    { path: '*', redirect: '/home' },
   ],
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,98 @@
 <template>
   <main>
     <h1>Home</h1>
-    <Search/>
+    <h3>Search for a class</h3>
+    <form action="javascript:void(0);" name="main">
+      <input type="text" v-model="Subject" placeholder="CSCI" required>
+      <select v-model="Term" required>
+        <option value="Winter">Winter</option>
+        <option value="Spring">Spring</option>
+        <option value="Summer">Summer</option>
+        <option value="Fall" selected>Fall</option>
+      </select>
+      <button @click="setParams()">Search</button>
+    </form>
+    <Donut v-if="this.requestMade"/>
+    <span v-else-if="this.resData === undefined">
+      <h3>There are no classes that match that search</h3>
+    </span>
+    <section v-else-if="this.resBody">
+      <Class v-for="item in this.resData" :key="item.CRN" :data="item"></Class>
+    </section>
   </main>
 </template>
 <script>
-import Search from '@/components/Search.vue';
+import Donut from '@/components/Donut.vue';
+import Class from '@/components/Class.vue';
+import finder from '@/finder';
 
 export default {
   name: 'Home',
   components: {
-    Search,
+    Class,
+    Donut,
+  },
+  data() {
+    return {
+      Subject: '',
+      Term: '',
+      resData: {},
+      resBody: '',
+      requestMade: false,
+    };
+  },
+  methods: {
+    setParams() {
+      if (this.Term && this.Subject) {
+        this.$router.push({ name: 'home', params: { term: this.Term, subject: this.Subject } });
+        this.search();
+      }
+    },
+    async search() {
+      // check to make sure form has correct elements
+      if (this.Term && this.Subject) {
+        this.resData = '';
+        this.requestMade = true;
+        const terms = {
+          Winter: 1,
+          Spring: 2,
+          Summer: 3,
+          Fall: 4,
+        };
+        // call the parser with the form data
+        const response = await finder(this.Subject, terms[this.Term]);
+        // store data to stop loading animation/show error if empty
+        this.resData = response.labeledChunks;
+        this.resBody = response.body;
+        this.requestMade = false;
+      }
+    },
+  },
+  mounted() {
+    // this is needed to prevent firefox from not auto selecting the correct select tag
+    document.forms.main.reset();
+
+    // if route params are set, use those instead
+    if (this.$route.params.term && this.$route.params.subject) {
+      this.Term = this.$route.params.term;
+      this.Subject = this.$route.params.subject;
+      this.search();
+    }
   },
 };
 </script>
+
+<style scoped>
+section {
+  width: 80%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+@media only screen and (max-width: 600px) {
+  section {
+    width: calc(100% - 32px);
+  }
+}
+</style>
